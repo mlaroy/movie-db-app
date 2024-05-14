f11de6fff1335bad7ed9541065eaa59660b34587
Michael LaRoy <mike.laroy@gmail.com>
2024-05-14 13:53:32 -0700

    add github link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -79,6 +79,7 @@ const Header = () => {
                                     </svg>
                                 </button>
                                 <ul className="flex flex-col gap-4 text-lg text-center">
+                                    <li><a href="https://github.com/mlaroy/movie-db-app" className="hover:text-yellow-500 transition">GitHub</a></li>
                                     <li><NavLink to="/" className="hover:text-yellow-500 transition">Home</NavLink></li>
                                     <li><NavLink to="/about" className="hover:text-yellow-500 transition">About</NavLink></li>
                                     <li><NavLink to="/favourites" className="hover:text-yellow-500 transition">Favourites</NavLink></li>
